39261c6340e6d18e451459f2a532e391b9d0561b
hianshul07 <hianshul07@gmail.com>
2024-02-18 16:58:19 +0530

    getting data from data.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,45 @@
 import Comment from './components/Comment';
+import data from './data.json'
 
-const data = {
-	amyRobson: {
-		profilePhoto: '/src/images/avatars/image-amyrobson.png',
-		userName: 'amyrobson',
-		commentTime: '1 month ago',
-		comment:
-			"Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
-	},
-};
+// const data = {
+// 	amyRobson: {
+// 		profilePhoto: '/src/images/avatars/image-amyrobson.png',
+// 		userName: 'amyrobson',
+// 		commentTime: '1 month ago',
+// 		comment:
+// 			"Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
+// 		likes: 4,
+// 	},
+// 	maxBlagun: {
+// 		profilePhoto: '/src/images/avatars/image-maxblagun.png',
+// 		userName: 'maxblagun',
+// 		commentTime: '2 weeks ago',
+// 		comment:
+// 			"Woah, your project looks awesome! How long have you been coding for? I'm still new. but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React?",
+// 		likes: 2,
+// 	},
+// };
 
 const App = () => {
 	return (
-		<div className='text-'>
+		<div className='p-4'>
 			<Comment
-				profilePhoto={data.amyRobson.profilePhoto}
-				userName={data.amyRobson.userName}
-        commentTime={data.amyRobson.commentTime}
-        comment={data.amyRobson.comment}
+				profilePhoto={data.comments[0].user.image.png}
+        
+				userName={data.comments[0].user.username}
+				commentTime={data.comments[0].createdAt}
+				// comment={data.amyRobson.comment}
+				// intialLikes={data.amyRobson.likes}
 			/>
+
+
+			{/* <Comment
+				profilePhoto={data.maxBlagun.profilePhoto}
+				userName={data.maxBlagun.userName}
+				commentTime={data.maxBlagun.commentTime}
+				comment={data.maxBlagun.comment}
+				intialLikes={data.maxBlagun.likes}
+			/> */}
 		</div>
 	);
 };
--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,51 +3,55 @@ import { Box, Flex, Button } from '@chakra-ui/react';
 import iconReply from '../images/icon-reply.svg';
 import { useState } from 'react';
 
-const Comment = ({userName, profilePhoto, commentTime, comment}) => {
-  const [count, setCount] = useState(0);
+const Comment = ({
+	userName,
+	profilePhoto,
+	commentTime,
+	comment,
 
-  const toggleCountInc = () => {
-    setCount((prevCount) => prevCount + 1);
-  };
+}) => {
+	const [count, setCount] = useState(0);
 
-  const toggleCountDec = () => {
-    setCount((prevCount) => prevCount - 1);
-  };
+	const toggleCountInc = () => {
+		setCount((prevCount) => prevCount + 1);
+	};
 
-  return (
-    <div className='m-4 p-8 shadow-xl rounded-md shadow-gray-300'>
-      <Flex className=' items-center' justify={'start'} gap={4}>
-        <img src={profilePhoto} alt='' className='w-16' />
-        <h1 className='text-2xl font-semibold'>{userName}</h1>
-        <p className='text-gray-500 text-lg'>{commentTime}</p>
-      </Flex>
-      <p className='text-gray-500 text-lg mt-6'>
-        {comment}
-      </p>
-      <div className='flex items-center justify-between mt-6'>
-        <div className='flex gap-4 p-2 w-1/5 rounded-xl justify-evenly items-center bg-[#edf2f7] '>
-          <button
-            className='text-2xl font-bold text-[#d1d0ed] active:text-black'
-            onClick={toggleCountInc}
-          >
-            +
-          </button>
-          <p className='flex text-[#5357b6] text-xl font-bold justify-center'>
-            {count}
-          </p>
-          <button
-            className='text-2xl font-bold text-[#d1d0ed] active:text-black'
-            onClick={toggleCountDec}
-          >
-            -
-          </button>
-        </div>
-        <div className='flex items-center gap-2'>
-          <img src={iconReply} alt='' className='h-5'/>
-          <p className='font-bold text-[#5357b6] text-2xl'>Reply</p>
-        </div>
-      </div>
-    </div>
-  );
+	const toggleCountDec = () => {
+		setCount((prevCount) => prevCount - 1);
+	};
+
+	return (
+		<div className=' p-8 shadow-md rounded-md '>
+			<Flex className=' items-center' justify={'start'} gap={4}>
+				<img src={profilePhoto} alt='' className='w-16' />
+				<h1 className='text-2xl font-semibold'>{userName}</h1>
+				<p className='text-gray-500 text-lg'>{commentTime}</p>
+			</Flex>
+			<p className='text-gray-500 text-lg mt-6'>{comment}</p>
+			<div className='flex items-center justify-between mt-6'>
+				<div className='flex gap-4 p-2 w-1/5 rounded-xl justify-evenly items-center bg-[#edf2f7] '>
+					<button
+						className='text-2xl font-bold text-[#d1d0ed] active:text-black'
+						onClick={toggleCountInc}
+					>
+						+
+					</button>
+					<p className='flex text-[#5357b6] text-xl font-bold justify-center'>
+						{count}
+					</p>
+					<button
+						className='text-2xl font-bold text-[#d1d0ed] active:text-black'
+						onClick={toggleCountDec}
+					>
+						-
+					</button>
+				</div>
+				<div className='flex items-center gap-2'>
+					<img src={iconReply} alt='' className='h-5' />
+					<p className='font-bold text-[#5357b6] text-2xl'>Reply</p>
+				</div>
+			</div>
+		</div>
+	);
 };
 export default Comment;
